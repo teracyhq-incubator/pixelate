2b88f42710afb47076b2e080fc83f95681b8553c
Dat Phan <datphan0110@gmail.com>
2014-06-02 14:11:00 +0700

    PILOT-2 | change pixelate code & add save & radius change button

--- app/demo/js/custom.js
+++ app/demo/js/custom.js
@@ -31,25 +31,73 @@
         return fileReader.readAsDataURL(file);
       },
       pixelateCanvas: function(canvas) {
+        var self = this;
         Main.mainPixelate = pixelate(canvas, {
           debug: false
         });
-        Main.mainPixelate._$selectorCanvas.parent().height(Main.mainPixelate._$selectorCanvas.height());
+
+        Main.mainPixelate
+                        .on('select:stop', function(x, y, selectedArea) {
+                          if (selectedArea.isEmpty()) {
+                            $('#radius').prop('disabled', true);
+                          } else {
+                            $('#radius').prop('disabled', false);
+                          }
+                        });
+
+        var selectorCanvas = Main.mainPixelate._$selectorCanvas;
+        selectorCanvas.parents('.pixelate-wrap').height(selectorCanvas.height());
+        selectorCanvas.parents('.pixelate-wrap').width(selectorCanvas.width());
+
+        $('#radius').change(function() {
+          $('#radius-value').html('(' + $(this).val() + ')');
+          Main.mainPixelate.options.radius = $(this).val();
+          Main.mainPixelate.mask();
+        });
+        
+        $('#save').click(function() {
+          if (Main.mainPixelate.isMasked()) {
+            self.apiPixelate();
+            $('#radius').prop('disabled', true);
+          }
+          Main.mainPixelate.currentCanvas.toBlob(function(blob) {
+              saveAs(blob, "pixelated-image.png");
+          });
+          return false;
+        });
+
         $('html, body').animate({
-          scrollTop: Main.mainPixelate._$selectorCanvas.parent().offset().top
+          scrollTop: selectorCanvas.parent().offset().top
         })
       },
       createImage: function(imgSrc) {
         if (!defaults.multiImage) {
           $('#upload-images *').remove();
         }
-        var w, h, img;
+        var w, h, img, controls, canvasWrap;
+        controls = $('<div><form class="form-horizontal" role="form">' +
+          '<div class="form-group">' +
+            '<label class="col-sm-3 control-label" for="radius">Radius <b id="radius-value">(10)</b></label>' +
+            '<div class="col-sm-9">' +
+              '<input disabled="disabled" type="range" min="1" max="100" value="10" class="form-control" id="radius" placeholder="Enter radius number bettwen 1 and 100.">' +
+            '</div>' +
+          '</div>' +
+          '<div class="form-group">' +
+            '<div class="col-sm-offset-3 col-sm-9">' +
+              '<a id="save" class="btn btn-default" href="#">Save</a>' +
+            '</div>' +
+          '</div>' +
+          '</form></div>');
+        canvasWrap = $('<div class="pixelate-wrap"></div>');
         Main.mainCavas = document.createElement('canvas');
         img = new Image();
-        $('#upload-images').append(Main.mainCavas);
+        
+        $('#upload-images').append(canvasWrap);
+        
         var wrapWidth = $('#upload-images').width()
 
         img.onload = function() {
+          canvasWrap.append(Main.mainCavas, controls);
           Main.mainContext = Main.mainCavas.getContext('2d');
           w = wrapWidth > img.width ? img.width : wrapWidth;
           h = img.height * w/img.width;
